4457d324df42ad795fa2f8b3111ab310b4127e83
Gurkirat Singh <gurkiratsingh2001@gmail.com>
2021-10-12 13:46:06 +0530

    Add ros

--- config/ros/custom-melodic/run_ros.bash
+++ config/ros/custom-melodic/run_ros.bash
@@ -1,10 +1,12 @@
 
-docker run -it \
+docker run -td \
+    -v ~/catkin_ws:/home/user/catkin_ws \
+    -v ~/IIIT-Stuff/RRC/rpvio/data:/home/user/data \
     --env="DISPLAY=$DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --runtime=nvidia \
-    --name=rrcros \
-    ros:nvidia \
-    bash
-
+    --name=rpvio \
+    --cap-add sys_ptrace \
+    -p127.0.0.1:2222:22 \
+    ros:nvidia
--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -x
-
-sudo apt update
